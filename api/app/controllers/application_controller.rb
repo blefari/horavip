327a632cc474d9f000a519f62b9c8375b96a7d7f
Lucas <lucas.loliveira@hotmail.com>
2015-03-24 09:55:22 -0300

    Fixing API

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+
+  respond_to :json
+  private
+  def record_not_unique
+    render :json => {:error => "record_not_unique"}, :status => 400
+  end
 end
